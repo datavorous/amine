4d8cdad28c96857864c62ca9207efbc1d4ee64b2
Sagnik <70896212+datavorous@users.noreply.github.com>
2024-08-28 15:37:31 +0530

    Delete focus_cli directory

--- focus_cli/utils.py
+++
@@ -1,7 +0,0 @@
-from tqdm import tqdm
-import time
-
-def countdown_timer(seconds):
-    """Countdown timer for the specified number of seconds."""
-    for remaining in tqdm(range(seconds), desc=f"Time left", bar_format="{l_bar}{bar} | {remaining}s left"):
-        time.sleep(1)
